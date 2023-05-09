b0413ff94b9ab94a2108df1c653c696908e3ff88
ali mert inal <mert.inal@gmail.com>
2023-05-09 17:42:35 +0200

    add vue-mastery-project

---
+++ real-app/project/src/components/EventCard.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineProps({
+  event: {
+    type: Object,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+  <RouterLink class="event-link" :to="{name: 'event-details', params:{id: event.id}}">
+  <div class="event-card">
+   <!-- Display Event Data -->
+    <h3>{{event.title}}</h3>
+    <p>@{{event.time}} and on {{event.date}}</p>
+    <p>at {{event.location}}</p>
+    <p>{{event.description}}</p>
+  </div>
+  </RouterLink>
+</template>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+.event-link{
+  text-decoration: none;
+  color: black;}
+</style>
---
+++ real-app/project/src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>The site for events where people can find challenges.</h1>
+  </div>
+</template>
+
+
