5de8d8b0585ec0085680b9949c45e79e578609ca
ali mert inal <mert.inal@gmail.com>
2023-05-09 18:32:04 +0200

    add path

--- real-app/project/vite.config.js
+++ real-app/project/vite.config.js
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from './@vitejs/plugin-v'
+import vue from './node_modules/@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
