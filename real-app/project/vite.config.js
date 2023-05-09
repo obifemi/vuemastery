754f600ff00e426d7619e4675c6b81a48a4248d5
ali mert inal <mert.inal@gmail.com>
2023-05-09 18:39:30 +0200

    add ed

--- real-app/project/vite.config.js
+++ real-app/project/vite.config.js
@@ -1,5 +1,4 @@
-import { fileURLToPath, URL } from 'node:url'
-
+import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite'
 import vue from './node_modules/@vitejs/plugin-vue'
 
