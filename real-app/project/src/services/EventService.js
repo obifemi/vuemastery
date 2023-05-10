bc5e1dcaca9e78b1fd43fcdfaa834d5b49b17711
ali mert inal <mert.inal@gmail.com>
2023-05-10 10:29:30 +0200

    add prev next buttons composition api

--- real-app/project/src/services/EventService.js
+++ real-app/project/src/services/EventService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const apiClient = axios.create({
-    baseURL: `https://my-json-server.typicode.com/obifemi/fake_json`,
+    baseURL: `https://my-json-server.typicode.com/Code-Pop/Touring-Vue-Router`,
     withCredentials: false,
     headers: {
         Accept: 'application/json',
@@ -9,10 +9,11 @@ const apiClient = axios.create({
 });
 
 export default {
-    getEvents() {
-        return apiClient.get('/events');
+    getEvents(perPage, page) {
+        return apiClient.get('/events?_limit=' + perPage + '&_page=' + page);
     },
     getEvent(id) {
         return apiClient.get('/events/' + id);
     }
 }
+
--- real-app/project/src/views/EventListView.vue
+++ real-app/project/src/views/EventListView.vue
@@ -1,24 +1,60 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watchEffect } from 'vue'
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
 const events = ref(null)
+const page = ref(1)
+defineProps({
+  page: Number,
+})
 onMounted(() => {
-  EventService.getEvents()
+  fetchEvents()
+})
+watchEffect(() => {
+  fetchEvents()
+})
+function fetchEvents() {
+  EventService.getEvents(2, page.value)
     .then((response) => {
       events.value = response.data
     })
     .catch((error) => {
       console.log(error)
     })
-})
+}
+function previousPage() {
+  if (page.value > 1) {
+    page.value--
+  }
+}
 </script>
 
 <template>
   <h1>Events For Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <router-link
+        id="page-prev"
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        @click="previousPage()"
+        >Previous</router-link
+      >
+
+      <router-link
+        id="page-next"
+        :to="{ name: 'event-list', query: { page: page + 1 } }"
+        rel="next"
+        @click="page++"
+        >Next</router-link
+      >
+
   </div>
+
+
+   
 </template>
 
 <style scoped>
