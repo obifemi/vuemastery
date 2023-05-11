ffc77befe82719c3b3162fe1894789487c362dbd
ali mert inal <mert.inal@gmail.com>
2023-05-11 12:14:16 +0200

    add computed

--- real-app/project/src/views/EventListView.vue
+++ real-app/project/src/views/EventListView.vue
@@ -1,8 +1,10 @@
 <script setup>
-import { ref, onMounted, watchEffect } from 'vue'
+import { ref, onMounted, watchEffect, computed } from 'vue'
+
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
 const events = ref(null)
+const totalEvents = ref(0)
 const page = ref(1)
 defineProps({
   page: Number,
@@ -11,12 +13,15 @@ onMounted(() => {
   fetchEvents()
 })
 watchEffect(() => {
+  events.value = null
   fetchEvents()
 })
 function fetchEvents() {
   EventService.getEvents(2, page.value)
     .then((response) => {
+      console.log(response)
       events.value = response.data
+      totalEvents.value = response.headers['x-total-count']
     })
     .catch((error) => {
       console.log(error)
@@ -27,34 +32,38 @@ function previousPage() {
     page.value--
   }
 }
+const hasNextPage = computed(() => {
+  var totalPages = Math.ceil(totalEvents.value / 2)
+  return page.value < totalPages
+})
+const hasPreviousPage = computed(() => {
+  return page.value > 1
+})
 </script>
 
 <template>
-  <h1>Events For Good</h1>
-  <div class="events">
+  <h1 >Events For Good</h1>
+  <p v-if="events===null">Events Loading...</p>
+  <div v-if="events!=null" class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
 
     <router-link
-        id="page-prev"
-        :to="{ name: 'event-list', query: { page: page - 1 } }"
-        rel="prev"
-        v-if="page != 1"
-        @click="previousPage()"
-        >Previous</router-link
-      >
+      v-if="hasPreviousPage"
+      id="page-prev"
+      :to="{ name: 'event-list', query: { page: page - 1 } }"
+      rel="prev"
+      @click="previousPage()"
+    >Previous</router-link>
 
-      <router-link
-        id="page-next"
-        :to="{ name: 'event-list', query: { page: page + 1 } }"
-        rel="next"
-        @click="page++"
-        >Next</router-link
-      >
+    <router-link
+      v-if="hasNextPage"
+      id="page-next"
+      :to="{ name: 'event-list', query: { page: page + 1 } }"
+      rel="next"
+      @click="page++"
+    >Next</router-link>
 
   </div>
-
-
-   
 </template>
 
 <style scoped>
