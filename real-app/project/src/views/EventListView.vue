4bb0617ab0e9aa1ac559a618fd51a6e12d10986b
ali mert inal <mert.inal@gmail.com>
2023-05-11 12:17:33 +0200

    add computed2

--- real-app/project/src/views/EventListView.vue
+++ real-app/project/src/views/EventListView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, watchEffect, computed } from 'vue'
+import { ref, onMounted, watchEffect,computed } from 'vue'
 
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
@@ -19,6 +19,7 @@ watchEffect(() => {
 function fetchEvents() {
   EventService.getEvents(2, page.value)
     .then((response) => {
+      
       console.log(response)
       events.value = response.data
       totalEvents.value = response.headers['x-total-count']
@@ -36,9 +37,6 @@ const hasNextPage = computed(() => {
   var totalPages = Math.ceil(totalEvents.value / 2)
   return page.value < totalPages
 })
-const hasPreviousPage = computed(() => {
-  return page.value > 1
-})
 </script>
 
 <template>
@@ -48,22 +46,27 @@ const hasPreviousPage = computed(() => {
     <EventCard v-for="event in events" :key="event.id" :event="event" />
 
     <router-link
-      v-if="hasPreviousPage"
-      id="page-prev"
-      :to="{ name: 'event-list', query: { page: page - 1 } }"
-      rel="prev"
-      @click="previousPage()"
-    >Previous</router-link>
+        id="page-prev"
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        @click="previousPage()"
+        >Previous</router-link
+      >
 
-    <router-link
-      v-if="hasNextPage"
-      id="page-next"
-      :to="{ name: 'event-list', query: { page: page + 1 } }"
-      rel="next"
-      @click="page++"
-    >Next</router-link>
+      <router-link
+        v-if="hasNextPage"
+        id="page-next"
+        :to="{ name: 'event-list', query: { page: page + 1 } }"
+        rel="next"
+        @click="page++"
+        >Next</router-link
+      >
 
   </div>
+
+
+   
 </template>
 
 <style scoped>
