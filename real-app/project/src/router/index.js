707dda4edaecd0d1030b05aa5ceca5e080c9cff7
ali mert inal <mert.inal@gmail.com>
2023-05-11 15:50:34 +0200

    add route nesting

--- real-app/project/src/router/index.js
+++ real-app/project/src/router/index.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
 import AboutView from '../views/AboutView.vue'
-import EventDetailsView from '../views/EventDetailsView.vue'
+import EventDetailsView from '../views/event/Details.vue'
+import EventRegisterView from '../views/event/Register.vue'
+import EventEditView from '../views/event/Edit.vue'
+import EventLayout from '../views/event/Layout.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,7 +14,7 @@ const router = createRouter({
       path: '/',
       name: 'event-list',
       component: EventListView,
-      props: route => ({ page: parseInt(route.query.page) || 1 })
+      props: (route) => ({ page: parseInt(route.query.page) || 1 })
     },
     {
       path: '/about',
@@ -18,15 +22,32 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: AboutView
+      component: AboutView,
     },
     {
       path: '/event/:id',
-      name: 'event-details',
-      component: EventDetailsView,
-      props: true
+      name: 'event-layout',
+      component: EventLayout,
+      props: true,
+      children: [
+        {
+          path: '',
+          name: 'event-details',
+          component: EventDetailsView,
+        },
+        {
+          path: 'register',
+          name: 'event-register',
+          component: EventRegisterView,
+        },
+        {
+          path: 'edit',
+          name: 'event-edit',
+          component: EventEditView,
+        }
+      ]
     }
-  ]
+  ],
 })
 
 export default router
